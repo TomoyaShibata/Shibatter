dbdb1886c9734cc10e0d556d9dd973f9e0969f4c
TomoyaShibata <TomoyaShibata>
2015-06-23 23:48:26 +0900

    [modify] MainPage と TweetPage を行き来できる処理を実装 [modify] TweetPage のテキストボックスに文字列が入っていた場合、前画面へ戻る前に確認ダイアログを表示する処理を実装。

--- Shibatter/Views/TweetPage.xaml.cs
+++ Shibatter/Views/TweetPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml.Navigation;
+﻿using System;
+using Windows.Phone.UI.Input;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Navigation;
 using Microsoft.Practices.Prism.Mvvm;
 using Microsoft.Practices.Prism.StoreApps;
 
@@ -21,8 +25,22 @@ namespace Shibatter.Views
         /// </summary>
         /// <param name="e">このページにどのように到達したかを説明するイベント データ。
         /// このプロパティは、通常、ページを構成するために使用します。</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        //protected override void OnNavigatedTo(NavigationEventArgs e)
+        //{
+        //}
+
+        private async void AppBarButtonCancel_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(TextBoxTweet.Text))
+            {
+                Frame.GoBack();
+                return;
+            }
+
+            var confirmDialog = new MessageDialog("ツイートを下書きに保存しますか？", "下書きの保存");
+            confirmDialog.Commands.Add(new UICommand("保存する"));
+            confirmDialog.Commands.Add(new UICommand("閉じる", (contextMenuCmd) => Frame.GoBack()));
+            await confirmDialog.ShowAsync();
         }
     }
 }
